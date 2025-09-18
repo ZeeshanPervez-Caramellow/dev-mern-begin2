25ff00db4109e62d85012b723fb842a953f3d87f
ZeeshanPervez-Caramellow <zeeshanpervez8@gmail.com>
2025-09-19 04:26:15 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import express from "express"
+const app = express();
+
+;const connectDB = async function() {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`DATABASE CONNECTED !!! HOST NAME : ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("ERR:",error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
